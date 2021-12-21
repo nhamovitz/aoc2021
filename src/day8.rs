c7ac6264c883caf76ac93ae7f6fb278145d7b1be
Nathaniel Hamovitz <18648574+nhamovitz@users.noreply.github.com>
2021-12-21 15:26:50 -0800

    day 8 setup

---
+++ src/day8.rs
@@ -0,0 +1,49 @@
+const INPUT: &str = include_str!("input/8.txt");
+
+fn get_input() -> Vec<u64> {
+    todo!()
+}
+
+fn part1() -> u64 {
+    todo!()
+}
+
+pub fn part1_pretty() {
+    println!("day XXX part 1: {}", part1());
+}
+
+fn part2() -> u64 {
+    todo!()
+}
+
+pub fn part2_pretty() {
+    println!("day XXX part 2: {}", part2());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t_part1() {
+        // assert_eq!(part1(), XXX);
+    }
+
+    #[test]
+    fn t_part2() {
+        // assert_eq!( , );
+    }
+
+    // extern crate test;
+    // use test::{black_box, Bencher};
+
+    // #[bench]
+    // fn b_part1(b: &mut Bencher) {
+    //     b.iter(|| part1());
+    // }
+
+    // #[bench]
+    // fn b_part2(b: &mut Bencher) {
+    //     b.iter(|| part2());
+    // }
+}
